3a7984f3213b601b2c04a99df5801f8368919062
Stephany <stephykpcarmo@outlook.com>
2023-09-19 00:17:36 -0300

    add: forgotten file

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,18 @@
+function Home() {
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          name=""
+          id=""
+        />
+      </div>
+      <h2 data-testid="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </h2>
+    </>
+  );
+}
+
+export default Home;
