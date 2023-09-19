5c130b448de5c366a70fbbbd1d0bc81ae9bb8e59
Maires Souza <trabalho.maires@gmail.com>
2023-09-18 22:36:30 -0300

    feat: API access module implemented

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,35 @@
 export async function getCategories() {
-  // Implemente aqui
+  const CATEGORY_API = 'https://api.mercadolibre.com/sites/MLB/categories';
+  try {
+    const getData = await fetch(CATEGORY_API);
+    const categoryData = await getData.json();
+    return categoryData;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function
+getProductsFromCategoryAndQuery(query: string, categoryId?: string) {
+  try {
+    const SEARCH_PRODUCT_API = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+    const getData = await fetch(SEARCH_PRODUCT_API);
+    const products = await getData.json();
+    return products;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-export async function getProductById() {
-  // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
-  // Atenção: essa função não deverá ser chamada na tela do carrinho de compras.
+// Esta implementação específica não é avaliada, mas pode ajudar você 🙂
+// Atenção: essa função não deverá ser chamada na tela do carrinho de compras.
+export async function getProductById(productId: string) {
+  try {
+    const PRODUCT_DETAILS_API = `https://api.mercadolibre.com/items/${productId}`;
+    const getData = await fetch(PRODUCT_DETAILS_API);
+    const details = await getData.json();
+    return details;
+  } catch (error) {
+    console.error(error);
+  }
 }
