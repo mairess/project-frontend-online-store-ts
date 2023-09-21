a1ba8b533f7ec7e5c48b8d6e0ca9ab3c186bbef8
Ludmilact <ludmilatimoteo@Outlook.com>
2023-09-20 21:59:56 -0300

    feat: product quantity manipulation

--- src/pages/shoppingCart/ShoppingCart.tsx
+++ src/pages/shoppingCart/ShoppingCart.tsx
@@ -1,33 +1,63 @@
 import { Link } from 'react-router-dom';
 import { BiArrowBack } from 'react-icons/bi';
+import { useEffect, useState } from 'react';
 import { TypeProductInCart } from '../../types';
 import { ProductsWrapper } from './styles';
 
 function ShoppingCart() {
-  const storage: TypeProductInCart[] = JSON.parse(localStorage.getItem('cart') || '[]');
+  const [storage, setStorage] = useState<TypeProductInCart[]>([]);
+  const [uniqueProducts, setUniqueProducts] = useState<TypeProductInCart[]>([]);
+  const [productQuantities, setProductQuantities] = useState<
+  { [productId: string]: number }>({});
 
-  const productQuantity = (productId: string) => {
-    return storage
-      .filter((product: TypeProductInCart) => product.id === productId).length;
+  useEffect(() => {
+    setStorage(JSON.parse(localStorage.getItem('cart') || '[]'));
+  }, []);
+
+  useEffect(() => {
+    const products = storage
+      .filter(
+        (product: TypeProductInCart, index: number, self: TypeProductInCart[]) => {
+          return index === self.findIndex((procutsFind) => procutsFind.id === product.id);
+        },
+      );
+    setUniqueProducts(products);
+    console.log(storage);
+    const myQuantities : { [productId: string]: number } = {};
+    storage.forEach((product: TypeProductInCart) => {
+      if (!myQuantities[product.id]) {
+        myQuantities[product.id] = 1;
+      } else {
+        myQuantities[product.id]++;
+      }
+    });
+    setProductQuantities(myQuantities);
+  }, [storage]);
+
+  // quando o product for alterado para mais ou menos o productQuantities precisa recalcular
+
+  // se clicar adicionar vou add o item no storage
+  const increseItem = (item : TypeProductInCart) => {
+    const increseItens = [...storage];
+    increseItens.push(item);
+    localStorage.setItem('cart', JSON.stringify(increseItens));
+    setStorage(increseItens);
   };
 
-  const productQuantities: { [productId: string]: number } = {};
+  // se clicar em remover remove um item no storage
+  const decreaseItem = (item: TypeProductInCart) => {
+    const decreaseStorage = [...storage];
+    const indexItem = decreaseStorage.indexOf(item);
+    decreaseStorage.splice(indexItem, 1);
+    setStorage(decreaseStorage);
+  };
 
-  storage.forEach((product: TypeProductInCart) => {
-    if (!productQuantities[product.id]) {
-      productQuantities[product.id] = 1;
-    } else {
-      productQuantities[product.id]++;
-    }
-  });
-
-  const uniqueProducts = storage
-    .filter(
-      (product: TypeProductInCart, index: number, self: TypeProductInCart[]) => {
-        return index === self.findIndex((procutsFind) => procutsFind.id === product.id);
-      },
-    );
-
+  // se clicar remove todo o item
+  const removerItem = (item: TypeProductInCart) => {
+    const itemRemove = storage.filter((element) => element.id !== item.id);
+    setStorage(itemRemove);
+    localStorage.setItem('cart', JSON.stringify(storage));
+  };
   return (
     <div>
       {/* link que retonar para a home */}
@@ -41,16 +71,16 @@ function ShoppingCart() {
       )}
       {uniqueProducts.map((item) => (
         <ProductsWrapper key={ item.id }>
-          <button>Remover</button>
+          <button onClick={ () => removerItem(item) }>Remover</button>
           <div><img src={ item.thumbnail } alt={ item.title } /></div>
           <div data-testid="shopping-cart-product-name">
             {item.title}
           </div>
-          <button>+</button>
+          <button onClick={ () => increseItem(item) }>+</button>
           <div data-testid="shopping-cart-product-quantity">
-            {productQuantity(item.id)}
+            {productQuantities[item.id]}
           </div>
-          <button>-</button>
+          <button onClick={ () => decreaseItem(item) }>-</button>
         </ProductsWrapper>
       ))}
     </div>
